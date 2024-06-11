b7d00b9978064c2cbd6c47db4ffbb716927cd05c
planetClaire <marshy@zeta.com.au>
2024-06-11 15:48:27 +0800

    Show primary menu by default

--- src/StarterSite.php
+++ src/StarterSite.php
@@ -49,7 +49,7 @@ class StarterSite extends Site
 		$context['foo']   = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
-		$context['menu']  = Timber::get_menu();
+		$context['menu']  = Timber::get_menu('primary');
 		$context['site']  = $this;
 
 		return $context;
